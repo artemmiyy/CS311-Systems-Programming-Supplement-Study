5694515b20ad6293579c36740b42bfe57695dbcc
Artemii Polshcha <artempolshcha@gmail.com>
2023-02-08 23:37:32 -0500

    finished implementation

--- flag_system.c
+++ flag_system.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
+#define FIRST_FLAG 0b1
+#define SECOND_FLAG 0b10
+#define THIRD_FLAG 0b10000000
 
 int main(int argc, char* argv[]) {
-    unsigned int flags;
-    
-    int x,y;
-    x = 1;
-    y = 0;
+    // using one variable to evaluate multiple statements
+    // I can store 32 features in one int (32 bits)
+    // memory efficient
 
-    if (x == 1) {
-        printf("1st feature is enabled\n")
-    }
+    unsigned int flags = FIRST_FLAG | SECOND_FLAG | THIRD_FLAG; // state of all flags
+
+    // 1st feature 00000000 00000000 00000000 00000001
+    // 2nd feature 00000000 00000000 00000000 00000010
+    // 3rd feature 00000000 00000000 00000000 10000000
+
+    // disabling second feature:
+    flags = flags & ~SECOND_FLAG;
+
+    // enabling second feature;
+    flags = flags | SECOND_FLAG;
+
+    if (flags & FIRST_FLAG) printf("1st feature is enabled\n");
+
+    if (flags & SECOND_FLAG) printf("2nd feature is enabled\n");
     
-    if (y == 1) {
-        printf("2nd feature is enabled\n")
-    }
+    if (flags & THIRD_FLAG) printf("3rd feature is enabled\n");
+    
     return 0;
 } 
